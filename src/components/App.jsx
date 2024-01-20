9813471216f950734d3bf139c3aae4e00038d96d
SnehaShisNasty <katirina18@gmail.com>
2024-01-20 17:11:11 +0000

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,83 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Form from './form/Form';
+import { ContactList } from './contactList/ContactList';
+import { Filter } from './filter/Filter';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const data = JSON.parse(localStorage.getItem('contacts'));
+
+    return data || [];
+  });
+
+  const [filter, setFilter] = useState('');
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (!firstRender.current) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }, [contacts]);
+  useEffect(() => {
+    firstRender.current = false;
+  }, []);
+
+  const addContact = ({ name, number }) => {
+    console.log(name, number);
+    const toLowerCaseName = name.toLowerCase();
+
+    if (contacts.some(item => item.name.toLowerCase() === toLowerCaseName)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts(prevContacts => {
+      return [...prevContacts, newContact];
+    });
+  };
+  const deleteContact = id => {
+    console.log(id);
+    setContacts(prevContacts => prevContacts.filter(item => item.id !== id));
+  };
+
+  const changeFitler = ({ target }) => setFilter(target.value);
+
+  const getVisibleContact = () => {
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalizedFilter = filter.toLowerCase();
+
+    const filteredBooks = contacts.filter(({ name, number }) => {
+      const normalizedTitle = name.toLowerCase();
+      const normalizedAuthor = number.toLowerCase();
+
+      return (
+        normalizedAuthor.includes(normalizedFilter) ||
+        normalizedTitle.includes(normalizedFilter)
+      );
+    });
+
+    return filteredBooks;
+  };
+
+  const data = getVisibleContact();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className={css.section}>
+      <h1 className={css.title}>Phonebook</h1>
+      <Form onSubmit={addContact}></Form>
+
+      <Filter value={filter} onChange={changeFitler}></Filter>
+      <h2 className={css.titleContacts}> Contacts</h2>
+      <ContactList data={data} onDeleteContact={deleteContact}></ContactList>
+    </section>
   );
 };
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import css from './Form.module.css';
+
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
+const Form = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...INITIAL_STATE });
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(state);
+    onSubmit({ ...state });
+    reset();
+  };
+
+  const reset = () => {
+    setState({ name: '', number: '' });
+  };
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+  return (
+    <form onSubmit={handleSubmit} className={css.form}>
+      <label htmlFor={nameInputId} className={css.label}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+          id={nameInputId}
+          required
+        />
+      </label>
+      <label htmlFor={numberInputId} className={css.label}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={state.number}
+          onChange={handleChange}
+          id={numberInputId}
+          required
+        />
+      </label>
+
+      <button type="submit" className={css.submit}>
+        Add Contact
+      </button>
+    </form>
+  );
+};
+
+export default Form;
